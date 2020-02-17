f25bc8e9d8648a02db3a701433274578af3f07ed
otsukayu-ta <abc.abbreviation.xyz@gmail.com>
2020-02-17 14:13:00 +0900

    自動更新　修正済

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -65,16 +65,14 @@ $(function(){
 
   var reloadMessages = function() {
     last_message_id = $('.messages__box:last').data("message-id");
-    console.log(last_message_id);
-    $.ajax({
+     $.ajax({
       url: "api/messages",
       type: 'get',
       dataType: 'json',
       data: {id: last_message_id}
     })
     .done(function(messages) {
-      console.log(messages)
-      if (messages.length !== 0) {
+       if (messages.length !== 0) {
         var insertHTML = '';
         $.each(messages, function(i, message) {
           insertHTML += buildHTML(message)
